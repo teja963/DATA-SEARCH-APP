2c5f4e5abd9b7c1f637a25416901c543a5cf05ac
teja963 <panasateja123@gmail.com>
2022-02-09 13:24:47 +0530

    Main activity Created

--- app/src/main/java/com/smartherd/msapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msapp/MainActivity.kt
@@ -3,9 +3,11 @@ package com.smartherd.msapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity: AppCompatActivity() {                          //First we need to initialize it
+    override fun onCreate(savedInstanceState: Bundle?) {           //We need to choose on create fun with single parameter
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)                     //We need to connect activity_main to MainActivity through setcontent
     }
+
+
 }

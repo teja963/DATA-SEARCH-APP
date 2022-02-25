fd291ee2b443031337392ee2bde2c28ad2b59142
teja963 <panasateja123@gmail.com>
2022-02-25 18:07:09 +0530

    Navigation through another explicit activity added

--- app/src/main/java/com/smartherd/msapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.smartherd.msapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,13 +14,20 @@ class MainActivity: AppCompatActivity() {                          //First we ne
         setContentView(R.layout.activity_main)                     //We need to connect activity_main to MainActivity through setcontent
 
         button_show_toast.setOnClickListener {
-            //After clicking button this fun works
-            Log.i("MainActivity", "Show Toast Pressed")
-
-            Toast.makeText(this, "Show Toast Pressed", Toast.LENGTH_SHORT).show() //To display the pop of button
-
-
+            Log.i("MainActivity", "Show Toast Pressed")   //For testing purpose
+            Toast.makeText(this, "Show Toast", Toast.LENGTH_SHORT).show() //To display the popup notifications
         }
+
+        button_search.setOnClickListener {
+            Log.i("MainActivity","Search Pressed")
+            val message: String = input_text.text.toString()   //Taking input from user
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent =  Intent(this, SearchActivity::class.java)             //To go from one activity to another activity
+            startActivity(intent)                                                           //It will start the activity
+        }
+
+
     }
 
 }

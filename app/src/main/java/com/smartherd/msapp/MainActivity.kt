f6c242dc1e8a63d290249255fd6c42cee651e8db
teja963 <panasateja123@gmail.com>
2022-02-25 20:27:41 +0530

    code refactored

--- app/src/main/java/com/smartherd/msapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msapp/MainActivity.kt
@@ -13,19 +13,7 @@ class MainActivity: AppCompatActivity() {                          //First we ne
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)                     //We need to connect activity_main to MainActivity through setcontent
 
-        button_show_toast.setOnClickListener {
-            Log.i("MainActivity", "Show Toast Pressed")   //For testing purpose
-            Toast.makeText(this, "Show Toast", Toast.LENGTH_SHORT).show() //To display the popup notifications
-        }
 
-        button_search.setOnClickListener {
-            Log.i("MainActivity","Search Pressed")
-            val message: String = input_text.text.toString()   //Taking input from user
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-
-            val intent =  Intent(this, SearchActivity::class.java)             //To go from one activity to another activity
-            startActivity(intent)                                                           //It will start the activity
-        }
 
 
     }
--- app/src/main/java/com/smartherd/msapp/SearchActivity.kt
+++ app/src/main/java/com/smartherd/msapp/SearchActivity.kt
@@ -1,12 +1,19 @@
 package com.smartherd.msapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class SearchActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val bundle: Bundle? = intent.extras    //using nullable bundle, bcz input can't be null
+        val input_message =  bundle!!.getString("input_message")
+        Toast.makeText(this, input_message, Toast.LENGTH_SHORT).show()
+
     }
 
 }
